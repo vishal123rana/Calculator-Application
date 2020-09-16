895b3f8151612da63350b89d28274d36e2d15536
vishal rana <vishalrana030201998@gmail.com>
2020-09-16 11:24:14 -0400

    Vishal Wednesday Changes

--- app/src/main/java/com/example/calculatorapplication/History.java
+++ app/src/main/java/com/example/calculatorapplication/History.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -61,5 +62,10 @@ public class History extends AppCompatActivity {
         builder.setTitle(title);
         builder.setMessage(message);
         builder.show();
+    }
+
+    public void recyclerView(View view) {
+        Intent intent = new Intent(History.this,RecycleView.class);
+        startActivity(intent);
     }
 }
